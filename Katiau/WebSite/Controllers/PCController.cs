6aec4bd28432e08b23d1cc01202948e8c3f4ff6f
Mesquini <mesquini@live.com>
2017-02-20 17:08:18 -0300

    Adicionando 3 views, Play, Xbox e PC.

---
+++ Katiau/WebSite/Controllers/PCController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class PCController : Controller
+    {
+        // GET: PC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
