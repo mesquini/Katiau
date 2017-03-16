a58e5bbfafe212b587d4bcb2c8f39cfbdff7f04d
Victor Rodrigues <victorluizrodrigues@gmail.com>
2017-03-16 13:49:50 -0300

    Dropdown menu e botao login adicionados na home.

--- Katiau/WebSite/JS/index.js
+++ Katiau/WebSite/JS/index.js
@@ -1,13 +1,13 @@
-$('input[type="submit"]').mousedown(function(){
+﻿$('input[type="submit"]').mousedown(function(){
   $(this).css('background', '#2ecc71');
 });
 $('input[type="submit"]').mouseup(function(){
   $(this).css('background', '#1abc9c');
 });
 
-$('#loginform').click(function(){
-  $('.login').fadeToggle('slow');
-  $(this).toggleClass('green');
+$('#loginform').click(function (){
+    $('.login').fadeToggle('slow');
+    $(this).toggleClass('green');
 });
 
 
