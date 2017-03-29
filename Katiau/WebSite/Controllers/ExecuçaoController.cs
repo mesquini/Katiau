2ed7a346caa8660cbabc008d45a3b7c6d821b31f
Ana Carolina <a.carolina9@Hotmail.com>
2017-03-29 15:55:39 -0300

    Criando a pagina execuçao do TechMundo

---
+++ Katiau/WebSite/Controllers/ExecuçaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class ExecuçaoController : Controller
+    {
+        // GET: Execuçao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
